dcca9647b8090f9560755df0ce10fa038bf84c3e
재권 <diwo02@naver.com>
2024-11-04 20:43:44 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    images: {
-      domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+  basePath: isProd ? '/dcsAI.github.io' : '',
+  assetPrefix: isProd ? '/dcsAI.github.io/' : '',
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
